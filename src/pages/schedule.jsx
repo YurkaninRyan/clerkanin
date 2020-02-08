0f6da53f21352d24be5e3776ffd0ba855ce03227
Ryan Yurkanin <ryurkanin@getguru.com>
2020-02-08 14:27:30 -0500

    more stuff for schedule

--- src/pages/schedule.jsx
+++ src/pages/schedule.jsx
@@ -3,6 +3,8 @@ import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component"
+import { FaRing, FaCocktail, FaUtensils } from "react-icons/fa"
+import { GiPublicSpeaker, GiMusicalNotes } from "react-icons/gi"
 
 import Layout, { LayoutConstrained } from "../components/Layout/Layout"
 import SEO from "../components/seo"
@@ -17,22 +19,42 @@ export default function Schedule() {
       <LayoutConstrained>
         <h1>September 26th, 2020</h1>
         <VerticalTimeline animate={false}>
-          <VerticalTimelineElement date="4:30pm - 5:00pm">
-            <h3 className="vertical-timeline-element-title">Ceremony</h3>
+          <VerticalTimelineElement
+            date="4:30pm - 5:00pm"
+            icon={<FaRing />}
+            iconStyle={{ backgroundColor: "var(--linen)" }}
+          >
+            <h4 className="vertical-timeline-element-title">Ceremony</h4>
           </VerticalTimelineElement>
-          <VerticalTimelineElement date="5:00pm - 6:00pm">
-            <h3 className="vertical-timeline-element-title">Cocktail Hour</h3>
+          <VerticalTimelineElement
+            date="5:00pm - 6:00pm"
+            icon={<FaCocktail />}
+            iconStyle={{ backgroundColor: "var(--linen)" }}
+          >
+            <h4 className="vertical-timeline-element-title">Cocktail Hour</h4>
           </VerticalTimelineElement>
-          <VerticalTimelineElement date="6:00pm - 7:00pm">
-            <h3 className="vertical-timeline-element-title">Dinner</h3>
+          <VerticalTimelineElement
+            date="6:00pm - 7:00pm"
+            icon={<FaUtensils />}
+            iconStyle={{ backgroundColor: "var(--linen)" }}
+          >
+            <h4 className="vertical-timeline-element-title">Dinner</h4>
           </VerticalTimelineElement>
-          <VerticalTimelineElement date="7:00pm - 8:00pm">
-            <h3 className="vertical-timeline-element-title">
+          <VerticalTimelineElement
+            date="7:00pm - 8:00pm"
+            icon={<GiPublicSpeaker />}
+            iconStyle={{ backgroundColor: "var(--linen)" }}
+          >
+            <h4 className="vertical-timeline-element-title">
               Speeches and First Dances
-            </h3>
+            </h4>
           </VerticalTimelineElement>
-          <VerticalTimelineElement date="8:00pm - 11:00pm">
-            <h3 className="vertical-timeline-element-title">Partying!</h3>
+          <VerticalTimelineElement
+            date="8:00pm - 11:00pm"
+            icon={<GiMusicalNotes />}
+            iconStyle={{ backgroundColor: "var(--linen)" }}
+          >
+            <h4 className="vertical-timeline-element-title">Partying!</h4>
           </VerticalTimelineElement>
         </VerticalTimeline>
       </LayoutConstrained>
