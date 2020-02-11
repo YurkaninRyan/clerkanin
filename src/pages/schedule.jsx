d8ee066bc6b8e0fcd1b399c5daa3908be776a50e
Ryan Yurkanin <ryurkanin@getguru.com>
2020-02-10 20:18:19 -0500

    more updates to copy

--- src/pages/schedule.jsx
+++ src/pages/schedule.jsx
@@ -25,6 +25,15 @@ export default function Schedule() {
             iconStyle={{ backgroundColor: "var(--linen)" }}
           >
             <h4 className="vertical-timeline-element-title">Ceremony</h4>
+            <h6 className="vertical-timeline-element-subtitle">Upstairs</h6>
+            <ul>
+              <li>Officiated by Pastor Michelle Kaufman</li>
+              <li>
+                There's no elevator in the building so you have to take the
+                stairs
+              </li>
+              <li>Alana and Ryan will both be keeping their names</li>
+            </ul>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             date="5:00pm - 6:00pm"
@@ -32,6 +41,11 @@ export default function Schedule() {
             iconStyle={{ backgroundColor: "var(--linen)" }}
           >
             <h4 className="vertical-timeline-element-title">Cocktail Hour</h4>
+            <h6 className="vertical-timeline-element-subtitle">Downstairs</h6>
+            <ul>
+              <li>Open bar with wine, beer, and two signature cocktails</li>
+              <li>Appetizer stations will be available for picking</li>
+            </ul>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             date="6:00pm - 7:00pm"
@@ -39,6 +53,10 @@ export default function Schedule() {
             iconStyle={{ backgroundColor: "var(--linen)" }}
           >
             <h4 className="vertical-timeline-element-title">Dinner</h4>
+            <h6 className="vertical-timeline-element-subtitle">Downstairs</h6>
+            <ul>
+              <li>Served buffet style, we will have vegetarian options</li>
+            </ul>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             date="7:00pm - 8:00pm"
@@ -48,6 +66,12 @@ export default function Schedule() {
             <h4 className="vertical-timeline-element-title">
               Speeches and First Dances
             </h4>
+            <h6 className="vertical-timeline-element-subtitle">Downstairs</h6>
+            <ul>
+              <li>Maid of Honor and Best Man will give speeches</li>
+              <li>Father/Daughter and Mother/Son dances</li>
+              <li>Alana and Ryan's first dance</li>
+            </ul>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             date="8:00pm - 11:00pm"
@@ -55,6 +79,15 @@ export default function Schedule() {
             iconStyle={{ backgroundColor: "var(--linen)" }}
           >
             <h4 className="vertical-timeline-element-title">Partying!</h4>
+            <h6 className="vertical-timeline-element-subtitle">
+              Upstairs will have music and dancing <br />
+              Downstairs will have food and mingling
+            </h6>
+            <ul>
+              <li>We promise the music will be good</li>
+              <li>Sam Wagner offered to break dance</li>
+              <li>Keith Clerkin will be playing the trumpet</li>
+            </ul>
           </VerticalTimelineElement>
         </VerticalTimeline>
       </LayoutConstrained>
